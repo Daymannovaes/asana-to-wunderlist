5b5880547ed74258e3df22cac4e81965555d4e5c
Dayman Novaes <daymannovaes@gmail.com>
2018-06-23 13:36:30 -0300

    add asana npm and wunderlist npm

--- test/asana.spec.js
+++ test/asana.spec.js
@@ -2,12 +2,14 @@ const { expect } = require('chai');
 const asana = require('../lib/asana');
 
 describe('asana.js', function() {
+    this.timeout(10000);
+
     describe('#getMe', function() {
         it('should work properly', function() {
             return asana.getMe().then(user => {
                 expect(user).to.be.an('object');
-                expect(user).to.have.nested.property('data.id');
-                expect(user).to.have.nested.property('data.email');
+                expect(user).to.have.property('id');
+                expect(user).to.have.property('email');
             });
         });
     });
