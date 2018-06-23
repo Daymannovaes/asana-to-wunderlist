3bc71b84c4cf0fa7451d11a25ed82f3475e5d2b0
Dayman Novaes <daymannovaes@gmail.com>
2018-06-23 00:13:19 -0300

    add test for config ENV VAR

--- test/config.spec.js
+++ test/config.spec.js
@@ -1,10 +1,27 @@
 const { expect } = require('chai');
-const config = require('../config');
+const configPath = '../config';
+let config;
 
 describe('config.js', function() {
+    const requireConfig = () => config = require(configPath);
+    const deleteConfig = () => delete require.cache[require.resolve(configPath)];
+
+    beforeEach(requireConfig);
+    afterEach(deleteConfig);
+
     it('should work without node env', function() {
         expect(config).to.be.an('object');
         expect(config).to.have.property('ASANA_ACCESS_TOKEN');
+    });
+
+    it('should work with node env', function() {
+        process.env.ASANA_ACCESS_TOKEN = 'some token';
+
+        deleteConfig();
+        requireConfig();
+
+        expect(config).to.be.an('object');
+        expect(config.ASANA_ACCESS_TOKEN).to.equal(process.env.ASANA_ACCESS_TOKEN);
     });
 });
 
